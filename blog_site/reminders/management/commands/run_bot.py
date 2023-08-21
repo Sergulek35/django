2444348c4f149cdfbdc4b54f258ead97a05f9d68
Sergulek35 <kaskov.S.P@yandex.ru>
2023-08-21 10:33:49 +0300

    django_CBV

--- blog_site/reminders/management/commands/run_bot.py
+++ blog_site/reminders/management/commands/run_bot.py
@@ -3,7 +3,7 @@ from os import getenv
 from dotenv import load_dotenv
 from django.core.management.base import BaseCommand
 from django.core.exceptions import ObjectDoesNotExist
-from reminders.models import Month, Day, Birthday_boy, User
+from reminders.models import Month, Day, Birthday_boy, User, Reminder
 
 load_dotenv()
 
@@ -16,7 +16,6 @@ month_list = ['Января', 'Февраля', 'Марта', 'Апреля', '�
 
 class Command(BaseCommand):
 
-
     def handle(self, *args, **options):
         add_bot()
 
@@ -25,12 +24,17 @@ class Command(BaseCommand):
 def examination(message):
     # print(message)
     bot.reply_to(message, 'команды:\nstart - Регистрация\n'
-                          'help - Помощь\n'
+                          'help - Информация\n'
                           'birthday - Список всех именинников\n'
+                          'reminder - Ваши сообщения, напоминания\n'
                           '--------------------------------\n'
-                          'Для добавления новых именинников \n'
+                          'Для добавления новых именинников:\n*\n'
                           'введите текст через пробел в формате\n'
-                          '(фамилия имя число месяц)\n(Иванов Иван 1 10)\n'
+                          'фамилия имя число месяц\nИванов Иван 1 10\n'
+                          '---------------------------------- \n'
+                          'Для добавления новых сообщений, напоминаний:\n*\n'
+                          'в скобках введите сообщение, затем через пробел число и месяц \n'
+                          '(сообщение) 1 10\n'
                           '🤝')
 
 
@@ -60,35 +64,59 @@ def birthday(message):
         bot.send_message(chat_id, i)
 
 
+@bot.message_handler(commands=['reminder'])
+def reminder(message):
+    user_chat = User.objects.get(user_chat=message.chat.id)
+    reminder = Reminder.objects.filter(user=user_chat)
+    for i in reminder:
+        chat_id = message.chat.id
+        bot.send_message(chat_id, i)
+
+
 def add_bot():
     @bot.message_handler(content_types=['text'])
     def send_text(message):
-        info = (message.text.title().split())
         chat_id = message.chat.id
+        if '(' in message.text and ')' in message.text:
+            mess_remind = message.text[message.text.find('(') + 1:message.text.find(')')]
+            info_rem = (message.text).split()
+
+            info = [mess_remind, info_rem[-2], info_rem[-1]]
+
+            info_rem.clear()
+
+        else:
+            info = (message.text.title().split())
         try:
-            if len(info) != 4:
-                bot.send_message(chat_id, 'Не верный формат ввода')
-                bot.send_message(chat_id, 'введите текст через пробел в формате\n'
-                                          '(фамилия имя число месяц)\n(Иванов Иван 1 10)\n')
-            elif info[2] == '0' or int(info[2]) > 31:
-                bot.send_message(chat_id, 'Не верный ввод ДНЯ рождения')
+            day = Day.objects.get(day=info[-2])
+            id = Month.objects.get(id=info[-1])
+            user_chat = User.objects.get(user_chat=message.chat.id)
 
-            else:
-
-                day = Day.objects.get(day=info[2])
-                id = Month.objects.get(id=info[3])
-                user_chat = User.objects.get(user_chat=message.chat.id)
+            if len(info) == 4:
 
                 surname_name = Birthday_boy.objects.filter(user=user_chat, surname=info[0], name=info[1]).first()
                 if not surname_name:
                     Birthday_boy.objects.create(month=id, day=day, name=info[1], surname=info[0]).user.add(user_chat.id)
-                    bot.send_message(chat_id, 'Именинник успешно добавлен!')
+                    bot.send_message(chat_id, 'Именинник успешно добавлен 😊')
                 else:
                     bot.send_message(chat_id, 'У вас уже есть такой именинник\nнужно сменить фамилию или имя')
+
+            elif len(info) == 3:
+                Reminder.objects.create(month=id, day=day, reminder=info[0]).user.add(user_chat.id)
+                bot.send_message(chat_id, 'Добавлено новое сообщение, напоминание 😊')
+
+            else:
+                bot.send_message(chat_id, 'Не верный формат ввода! 😔')
+                bot.send_message(chat_id, 'для получения информации о правильном вводе\n'
+                                          ' нужно вызвать в телеграм боте команду (иформация)/help')
+
         except ValueError:
-            bot.send_message(chat_id, 'Число и месяц нужно вводить цифрами')
+            bot.send_message(chat_id, 'Нет даты или дата введена не цифрами 😔')
         except ObjectDoesNotExist:
-            bot.send_message(chat_id, 'Не верный ввод МЕСЯЦА рождения')
+            bot.send_message(chat_id, 'Не верный ввод ДНЯ или МЕСЯЦА рождения 😔')
+        except IndexError:
+            bot.send_message(chat_id, 'Не верный формат ввода 😔\n'
+                                'всего одно слово или буква')
 
         info.clear()
 
